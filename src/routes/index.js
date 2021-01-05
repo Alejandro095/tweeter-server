661b3988a4fc3707596e008485b20ce40df654a1
Alejandro Tovar <javi.monsi2009@hotmail.com>
2021-01-04 21:02:37 -0600

    Se cambio el tipo de la ruta auth/login a all por pruebas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const { ErrorController } = require("./../controllers/error")
 const PREFIX = "/api/v1"
 const ROUTES = [
     // Auth routes
-    { type:"post",  controller: LoginController, path: "/auth/login"},
+    { type:"all",  controller: LoginController, path: "/auth/login"},
     { type:"post",  controller: LoginController, path: "/auth/register"},
     { type:"get",   controller: LoginController, path: "/auth/renew"},
 
